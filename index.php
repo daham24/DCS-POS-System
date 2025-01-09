211cfe6feae497e4f895344bc17a99c65a15c284
daham24 <bhanukadaham98@gmail.com>
2025-01-09 10:51:01 +0530

    First commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php include("includes/header.php");?>
+
+<div class="welcome-container">
+  <h1 class="welcome-title">Dimuthu Cellular Service</h1>
+  <p class="welcome-description">
+    Body text for whatever you'd like to say. Add main takeaway points, quotes,
+    anecdotes, or even a very very short story.
+  </p>
+  <div class="button-container">
+    <button class="btn btn-primary" tabindex="0">Login</button>
+  </div>
+  <img
+    loading="lazy"
+    src="https://cdn.builder.io/api/v1/image/assets/TEMP/654a26b17a81e6ce82d734845f2c366cfee884002396d68461a460731a7ce5a7?apiKey=2758efc56d724d1aacd00d329c35c80b&"
+    class="welcome-image"
+    alt="Dimuthu Cellular Service welcome image"
+  />
+</div>
+
+<?php include("includes/footer.php");?>
+
+
+   
